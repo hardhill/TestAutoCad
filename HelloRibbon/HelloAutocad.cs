096105b4889fbd6c9e5cdd3bc8cb5704d13c858f
Белецкий Вячеслав <bva@sibdigital.net>
2022-12-06 20:51:19 +0800

    Добавьте файлы проекта.

---
+++ HelloRibbon/HelloAutocad.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using HelloCad.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloRibbon
+{
+    public class HelloAutocad:IExtensionApplication
+    {
+
+        private HelloWindow helloWindow;
+        public HelloAutocad()
+        {
+            helloWindow = new HelloWindow();
+        }
+
+        [CommandMethod("TestHello")]
+        public void TestHello()
+        {
+            var editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Привет из Autocad плагина");
+            helloWindow.Show();
+        }
+
+        public void Initialize()
+        {
+            var editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Инициализация плагина.." + Environment.NewLine);
+        }
+
+        public void Terminate()
+        {
+            
+        }
+    }
+}
